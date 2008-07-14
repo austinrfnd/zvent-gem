aab5f5d94b1a81035c93a65c168ce9307ade524b
Austin Fonacier <austinrf@gmail.com>
2008-07-13 19:20:54 -0700

    More skeleton work and more pushing

--- lib/zvent/session.rb
+++ lib/zvent/session.rb
@@ -3,5 +3,16 @@ module Zvent
     def initialize(api_key)
       @api_key = api_key
     end
+    
+    # find events 
+    # returns an array of events
+    def find_events(location, options = {})
+    end
+    
+    # find an event
+    # returns a single event if successful
+    # returns nil if nothing found
+    def find_event(event_id, options = {})
+    end
   end
 end
