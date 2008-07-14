de0fa7199b3ba886eb34c31729d0edf9151049d1
Austin Fonacier <austinrf@gmail.com>
2008-07-13 19:08:04 -0700

    Forming the skeleton.  A rather useless commit actually.  I just wanted to push stuff to github.

---
+++ lib/zvent.rb
@@ -0,0 +1,8 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+
+require 'rubygems'
+require 'json'
+require 'net/http'
+Dir[File.dirname(__FILE__) + "/zvent/*.rb"].each { |file| require(file) }
---
+++ lib/zvent/session.rb
@@ -0,0 +1,7 @@
+module Zvent
+  class Session
+    def initialize(api_key)
+      @api_key = api_key
+    end
+  end
+end
